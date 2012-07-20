6f7f65f5758d44999ba3348a25bc4a650470d498
Se Hoon Park <sheep@sehoonpark.com.ar>
2012-07-20 14:33:09 -0300

    Fixed bug introduced with friendly_id in category show(pagination related.)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 # -*- encoding : utf-8 -*-
 TreeExample::Application.routes.draw do
-  get 'categories/:id/page/:page', to: 'categories#show'
   get 'companies/page/:page', to: 'companies#index'
   get "static_pages/help"
   get "static_pages/about"
