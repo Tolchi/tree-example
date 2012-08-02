79928cd1d9ac7cebecf0f1d2dfe7420ba43ddce7
Se Hoon Park <sheep@sehoonpark.com.ar>
2012-08-02 11:13:17 -0300

    Fixed a typo introduced in last commit.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -23,7 +23,8 @@ class CompaniesController < ApplicationController
     @json = @company.to_gmaps4rails
     @tags = @company.tag_list
     ariane.add @company.name, @company
-    @keyword = String.new << @company.name << " " << @company.category.name << " " 
+    @keyword = String.new 
+    @keyword << @company.name << " " << @company.category.name << " " 
     @keyword << @tags.to_s.gsub!(/,/, "") unless @tags.blank?
   end
 
